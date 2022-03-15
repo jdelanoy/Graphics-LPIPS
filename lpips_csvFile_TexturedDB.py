906dca7744e472657058e0ff75d228441d6d9e0a
Johanna Delanoy <johanna.delanoy@insa-lyon.fr>
2022-03-15 17:22:41 +0100

    add model in output csv

--- lpips_csvFile_TexturedDB.py
+++ lpips_csvFile_TexturedDB.py
@@ -56,7 +56,7 @@ if(opt.use_gpu):
     
 ## Output file
 f = open(opt.output_dir+"/GraphicsLPIPS_TestsetScores.csv",'w')
-f.writelines('p0,lpips_alex,MOS,var_score,var_weight,entropy_score,entropy_weight, spearm corr, pears corr\n')
+f.writelines('model,p0,lpips_alex,MOS,var_score,var_weight,entropy_score,entropy_weight, spearm corr, pears corr\n')
 
 ## read Input csv file 
 List_MOS = []
@@ -151,7 +151,7 @@ with open(opt.csvfile) as csv_file:
                 #print((patches, patches_id),outputs)
                 plot_patches(opt.output_dir, 0, ([patches], [patches_id]), outputs, f"test_", stimulus=images, have_weight=opt.weight_patch, multiview=opt.multiview)
 
-            f.writelines('%s, %.6f, %s, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n'%(dist,MOSpredicted,MOS,var_score,var_weight,entropy_score,entropy_weight, spearm, pears))
+            f.writelines('%s, %s, %.6f, %s, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n'%(model,dist,MOSpredicted,MOS,var_score,var_weight,entropy_score,entropy_weight, spearm, pears))
             line_count +=1
 
 
